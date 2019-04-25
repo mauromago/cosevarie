be767e6566f7b5a5591b8d917b467997f8daedc5
mauromago <3030wr@gmail.com>
2019-04-25 16:10:57 +0200

    Add files via upload

---
+++ pluvimentro_1.0.ino
@@ -0,0 +1,98 @@
+//Pluvimentro versione 1.0
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+#define Spin D2 //sensor pin
+
+volatile int check = 0;
+const char* ssid = "BASE";
+const char* password = "";
+int interruptCounter = 0;
+int numberOfInterrupts = 10;
+const char* mqtt_server = "192.168.1.131";
+const char* mqtt_username = "";
+const char* mqtt_password = "";
+const char* mqtt_topic = "pioggia";
+const char* mqtt_topic2 = "debug";
+unsigned long oldNow;
+unsigned long testTime;
+const unsigned long RstTime = 3 * 60 * 1000;
+const unsigned long sndTime = 6 * 60 * 1000;
+bool stopped = false; 
+char strValore[2] = "X";
+
+IPAddress subnet(255, 255, 255, 0); // set subnet mask to match your network
+IPAddress ip(192, 168, 1, 216); // controllare che non sia doppio
+IPAddress gateAway(192, 168, 1, 254);
+IPAddress DNS(8, 8, 8, 8);
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup_wifi() {
+  delay(10);
+  WiFi.config(ip, gateAway, subnet);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    if (client.connect("ESP8266Client", mqtt_username, mqtt_password)) {
+    } else {
+    delay(5000);
+    }
+  }
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+}
+
+void Hint () {
+  check = 1;
+  }
+
+void setup() {
+  attachInterrupt(Spin, Hint, FALLING);
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+  oldNow = millis();
+  testTime = millis();
+  setup_wifi();
+  reconnect();
+  client.publish(mqtt_topic2, "reset");
+}
+
+void loop() {
+  if (check){
+    detachInterrupt(Spin);  
+    itoa (interruptCounter, strValore, 10);
+    interruptCounter++;
+    if (interruptCounter >= numberOfInterrupts) {
+      interruptCounter = 0;  }
+    if (WiFi.status() != WL_CONNECTED) {
+      setup_wifi();
+    }
+    if (!client.connected()) {
+      reconnect(); }
+    client.publish(mqtt_topic, strValore);
+    client.disconnect();
+    delay(100);
+    check = 0;
+    oldNow = millis();
+    testTime = millis();
+    stopped = false;
+    attachInterrupt(Spin, Hint, FALLING);
+  }
+  delay(0);
+  if ((millis()-oldNow)> RstTime && !stopped) {
+   if (!client.connected()) {
+      reconnect(); } 
+   client.publish(mqtt_topic2, "disconnet");
+   espClient.stop();
+   stopped = true;
+  }
+}
+
