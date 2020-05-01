99be3fa6eebf9b76f9f4f44b6f0e33cf95b712d4
mauromago <3030wr@gmail.com>
2020-05-01 22:47:56 +0200

    Add files via upload

---
+++ setmylan (copia 1).h
@@ -0,0 +1,12 @@
+
+IPAddress subnet(255, 255, 255, 0); 
+IPAddress gateAway(192, 168,--,--);
+IPAddress DNS(8, 8, 8, 8);
+
+const char* ssid = "---";
+const char* password = "";
+const char* mqtt_username = "";
+const char* mqtt_password = "";
+
+#define MQTT_HOST IPAddress(--,--,--,--)
+#define MQTT_PORT 1883
